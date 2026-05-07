0274f71628293468e441c1a0771f2cb67e566143
Nagachaithanyagajjala <144054962+Nagachaithanyagajjala@users.noreply.github.com>
2026-05-07 14:25:01 +0530

    Remove 'Bearer' prefix from Authorization header

--- apps/Web/src/hooks/useTeamMembers.js
+++ apps/Web/src/hooks/useTeamMembers.js
@@ -21,7 +21,7 @@ export const useTeamMembers = () => {
         "/users",
         {
           headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: token,
           },
         }
       );
